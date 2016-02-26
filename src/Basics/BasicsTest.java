6061cdf75cad1f71836afcedb76e43b858602fa7
npotli <npotli@gmail.com>
2016-02-25 21:13:39 -0500

    InterviewQuestionsPrep

---
+++ src/Basics/BasicsTest.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class BasicsTest  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IDbClass cls;
+		
+		cls = new SQLdbClass();
+		cls.addEmpl();
+		
+		//System.out.println("Hello World");
+	}
+
+}
---
+++ src/Basics/SQLdbClass.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class SQLdbClass implements IDbClass {
+	@Override
+	public void addEmpl(){
+		System.out.println("SqlDbClass");
+	}
+
+}
